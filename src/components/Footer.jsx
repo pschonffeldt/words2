fcf0032c020175ebfa0d8c16552fae0dbe96a55e
Pablo S <cineschonffeldt@gmail.com>
2025-06-05 17:39:44 -0500

    First commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>&copy: Copyright by pschonffeldt. All rights reserved.</small>
+      <small>Last cheked limits: 17 days ago</small>
+    </footer>
+  );
+}
